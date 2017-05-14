6a3e70b5ce8e3a2c2e01ad44283d7dfe4beeec7a
Sascha Timme <sascha.timme@gmail.com>
2017-05-14 21:44:29 +0200

    Update RNPlayground scripts

--- RNPlayground/watch.js
+++ RNPlayground/watch.js
@@ -10,18 +10,28 @@ function copyFile(src, dest) {
     });
 
     readStream.once("end", () => {
-        console.log("done copying");
+        // console.log("done copying");
     });
 
     readStream.pipe(fs.createWriteStream(dest));
 }
 
 function copyReactNavigation() {
-    const re = "reactNavigation.re";
-    const rei = "reactNavigation.rei";
+    const fileNames = [
+        "navigationActionsRe.re",
+        "reactNavigation.re",
+        "stackNavigatorRe.rei",
+        "tabNavigatorRe.rei",
+        "navigationRe.re",
+        "stackNavigatorRe.re",
+        "tabNavigatorRe.re",
+        "utils.re"
+    ];
 
-    copyFile(path.resolve(`../re/${re}`), path.resolve(`./reactNavigation/${re}`));
-    copyFile(path.resolve(`../re/${rei}`), path.resolve(`./reactNavigation/${rei}`));
+    fileNames.forEach (name => {
+        copyFile(path.resolve(`../re/${name}`), path.resolve(`./reactNavigation/${name}`));
+
+    }) 
 }
 
 const watcher = chokidar.watch("../re/*");
